0ec55746aea38963eec58456ebf6b797c3870ae7
yan <ianromanenko@gmail.com>
2017-09-20 22:05:51 -0400

    Added partial functianallity

--- MAPD714-F2017-Intermediate-User-Interactions/ViewController.swift
+++ MAPD714-F2017-Intermediate-User-Interactions/ViewController.swift
@@ -3,7 +3,7 @@
  * Date: September 20,2017
  * Student ID:
  * Description: Intermediate user interactions demo
- * Version: 0.2 - Added Outlets and actions
+ * Version: 0.3 - Added partial functianallity
  */
 
 
@@ -16,6 +16,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var sliderLabel: UILabel!
     
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+    
+    @IBOutlet weak var doSomethingButton: UIButton!
     
     //methods
     
@@ -26,16 +30,42 @@ class ViewController: UIViewController {
 
     
     // action methods
+    @IBAction func textFieldDoneEditing(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    @IBAction func onTapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        numberTextField.resignFirstResponder()
+    }
+    
     @IBAction func onSliderChanged(_ sender: UISlider) {
+        sliderLabel.text = String(lroundf(sender.value))
     }
 
     @IBAction func onSegmentedControlChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            leftSwitch.isHidden = false
+            rightSwitch.isHidden = false
+            doSomethingButton.isHidden = true
+        }
+        else {
+            leftSwitch.isHidden = true
+            rightSwitch.isHidden = true
+            doSomethingButton.isHidden = false
+        }
     }
 
     @IBAction func onDoSomethingPressed(_ sender: UIButton) {
     }
 
     @IBAction func onSwitchChanged(_ sender: UISwitch) {
+        
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+        
+        
+
     }
 
 
